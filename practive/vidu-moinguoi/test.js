c3b3b179c3b197b4c912a8bed5b45d9e5acf4276
mynguyen2712 <nguyenquangmy95dn@gmail.com>
2022-12-12 00:12:53 +0700

    homework1

---
+++ practive/vidu-moinguoi/test.js
@@ -0,0 +1,8 @@
+const print = console.log
+let firstName = 'My'
+const list_name = ['myNguyen','BinKiet','Trung','concac',]
+print(list_name[0])
+print(list_name[1])
+print(list_name[2])
+print(list_name[3])
+print(list_name[4])
